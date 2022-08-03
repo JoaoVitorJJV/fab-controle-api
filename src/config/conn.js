13ee9c2df5eb7053415f310ee1d178c552fa01d3
Joao Vitor <joaovitorp42@gmail.com>
2022-08-02 22:34:32 -0300

    ServerDB

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,22 +1,22 @@
 import {Sequelize} from 'sequelize'
 
 
-const instance = new Sequelize(
-  'fab', 
-  'root', 
-  '', 
-  {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
 // const instance = new Sequelize(
-//   'exbrhb80_exercito', 
+//   'fab', 
 //   'root', 
 //   '', 
 //   {
 //     host: 'localhost',
 //     dialect: 'mysql'
 // })
+
+const instance = new Sequelize(
+  'pvFnBSJWAn', 
+  'pvFnBSJWAn', 
+  'xDvdg65ZLL', 
+  {
+    host: 'remotemysql.com',
+    dialect: 'mysql'
+})
 
 export default instance
